6432b1cc9538cd404eff3f1bb0b4007e9a7b6d79
unknown <lakshmehta06@gmail.com>
2021-04-18 23:58:02 +0530

    [Laksh added para frequency]

--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -12,6 +12,9 @@ namespace HashTableProblem
             MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
             hash.Add("0", "To be or not to be");
             hash.frequencyOfWords("0");
+            hash.Add("1", "A random paragraph can also be an excellent way for a writer to tackle writers' block. Writing block can often happen due to being stuck with a current project that the writer is trying to complete. By inserting a completely random paragraph from which to begin, it can take down some of the issues that may have been causing the writers' block in the first place.");
+            hash.frequencyOfWords("1");
+
         }
     }
 }
