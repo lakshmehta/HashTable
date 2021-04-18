27e08f54a0625f804d4591101821a37ba2e1ae97
unknown <lakshmehta06@gmail.com>
2021-04-18 23:50:11 +0530

    [Laksh added WORD Frequency]

--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -6,7 +6,12 @@ namespace HashTableProblem
     {
         static void Main(string[] args)
         {
+          
             Console.WriteLine("Hello World!WELCOME TO HASHTABLE");
+            Console.WriteLine("Hash table demo"); //() []
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To be or not to be");
+            hash.frequencyOfWords("0");
         }
     }
 }
